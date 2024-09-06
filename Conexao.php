497a7b04dd1a2576b0cd6d6e056580bbb2b227f2
Joao_Vitor <joaovitorfernandescosta01@gmail.com>
2024-09-06 07:49:50 -0300

    conexao banco virtual

--- Conexao.php
+++ Conexao.php
@@ -1,9 +1,9 @@
 <?php
-$host="localhost";
-$user="root";
-$pass="";
-$banco="BDGenesFlix";
+$host="sql.freedb.tech";
+$user="freedb_Joao_Vitor";
+$pass="4Vpauh#e$5&bghN";
+$banco="freedb_GenesFlix";
 $conexao=mysqli_connect($host, $user, $pass,
 $banco) or die(mysql_error());
-mysqli_select_db($banco) or die(mysql_error());
+
 ?>
